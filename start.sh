ff4bb0f700f9bf62e75efa0b173b8621a42baee6
jbfink <john.fink@gmail.com>
2013-08-01 15:04:50 -0400

    Works! Yay!

--- start.sh
+++ start.sh
@@ -2,9 +2,13 @@
 /usr/sbin/mysqld & 
 sleep 10s
 MYSQL_PASSWORD=`pwgen -c -n -1 12`
+WORDPRESS_PASSWORD=`pwgen -c -n -1 12`
 echo mysql root password: $MYSQL_PASSWORD
+echo wordpress password: $WORDPRESS_PASSWORD
 echo $MYSQL_PASSWORD > /mysql-root-pw.txt
+echo $WORDPRESS_PASSWORD > /wordpress-db-pw.txt
 mysqladmin -u root password $MYSQL_PASSWORD 
+mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE wordpress; GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY '$WORDPRESS_PASSWORD'; FLUSH PRIVILEGES;"
 killall mysqld
 sleep 10s
 supervisord -n
