607a958854622e8c6ece0e758842ba7017c3c5c2
jbfink <john.fink@gmail.com>
2013-08-01 12:54:39 -0400

    Thing in start.sh to write mysql root pw to a file.

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 sleep 10s
 MYSQL_PASSWORD=`pwgen -c -n -1 12`
 echo mysql root password: $MYSQL_PASSWORD
+echo $MYSQL_PASSWORD > /mysql-root-pw.txt
 mysqladmin -u root password $MYSQL_PASSWORD 
 killall mysqld
 sleep 10s
