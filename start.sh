3b7cbaecc5a7c7a8406d466b39266ac2bf8afbd7
jbfink <john.fink@gmail.com>
2013-08-02 11:12:41 -0400

    took enviro variables for salt values out of start.sh because we're generating them automatically

--- start.sh
+++ start.sh
@@ -5,14 +5,6 @@ sleep 10s
 # Here we generate random passwords (thank you pwgen!). The first two are for mysql users, the last batch for random keys in wp-config.php
 MYSQL_PASSWORD=`pwgen -c -n -1 12`
 WORDPRESS_PASSWORD=`pwgen -c -n -1 12`
-AUTH_KEY=`pwgen -sy -1 65`
-SECURE_AUTH_KEY=`pwgen -sy -1 65`
-LOGGED_IN_KEY=`pwgen -sy -1 65`
-NONCE_KEY=`pwgen -sy -1 65`
-AUTH_SALT=`pwgen -sy -1 65`
-SECURE_AUTH_SALT=`pwgen -sy -1 65`
-LOGGED_IN_SALT=`pwgen -sy -1 65`
-NONCE_SALT=`pwgen -sy -1 65`
 #This is so the passwords show up in logs. 
 echo mysql root password: $MYSQL_PASSWORD
 echo wordpress password: $WORDPRESS_PASSWORD
